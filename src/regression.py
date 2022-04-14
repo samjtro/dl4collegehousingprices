a3d15baf439954ac0c70141f9fa9262bcd04d749
sam <samuel.troyer.j@gmail.com>
2022-04-13 19:30:20 -0600

    changing analysis

--- src/regression.py
+++ src/regression.py
@@ -5,24 +5,25 @@ from data.scrape import scrape as s
 from data.preprocess import preprocess as pp
 from sklearn.linear_model import LinearRegression
 
-
-s.__realtor__('Colorado','type-single-family-home/price-na-1000000',25)
+#s.__realtor__('Colorado','type-single-family-home/price-na-1000000',25)
 data = pd.read_csv("data/active_homes.csv")
-
+df = pd.read_csv("data/realtor.csv")
 df2 = pp.__df__(data)
 
 for i in range(len(df2)):
-    row = df2.iloc[i]
+    	row = df2.iloc[i]
     
-    blob = {
-        'address': row['Address'],
-        'price': row['Price'],
-        'bed': row['Bed'],
-        'bath': row['Bath'],
-        'sqft': row['Sqft']
-    }
+    	blob = {
+        	'address': row['Address'],
+        	'price': row['Price'],
+        	'bed': row['Bed'],
+        	'bath': row['Bath'],
+        	'sqft': row['Sqft']
+    	}
 
-
-df = pd.read_csv("data/realtor.csv")
-
-print(df)
+for i in range(len(df)):
+	row = df.iloc[i]
+	if(row['median_listing_price_yy']>=2):
+		print(row)
+	
+print(df['median_listing_price_mm'].max())
