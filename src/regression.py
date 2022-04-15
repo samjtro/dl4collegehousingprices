554636c5d7ed36c7fbb97ad8699ac23526c194cb
sam <samuel.troyer.j@gmail.com>
2022-04-14 22:12:15 -0600

    comprehensive rewrites

--- src/regression.py
+++ src/regression.py
@@ -1,29 +1,10 @@
 import torch as t
 import pandas as pd
 import numpy as np
-from data.scrape import scrape as s
-from data.preprocess import preprocess as pp
-from sklearn.linear_model import LinearRegression
 
-#s.__realtor__('Colorado','type-single-family-home/price-na-1000000',25)
-data = pd.read_csv("data/active_homes.csv")
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 df = pd.read_csv("data/realtor.csv")
-df2 = pp.__df__(data)
 
-for i in range(len(df2)):
-    	row = df2.iloc[i]
-    
-    	blob = {
-        	'address': row['Address'],
-        	'price': row['Price'],
-        	'bed': row['Bed'],
-        	'bath': row['Bath'],
-        	'sqft': row['Sqft']
-    	}
-
-for i in range(len(df)):
-	row = df.iloc[i]
-	if(row['median_listing_price_yy']>=2):
-		print(row)
-	
-print(df['median_listing_price_mm'].max())
+X,y = 
--- src/scrape.py
+++ src/scrape.py
@@ -5,12 +5,7 @@ from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-df = pd.DataFrame(columns=['Address','Status','Price','Meta'])
-
-statuses = []
-prices = []
-metas = []
-addresses = []
+#('Colorado','type-single-family-home/price-na-1000000',25)
 
 class scrape:
         def __init__(self):
@@ -28,6 +23,16 @@ options -
 
 pages - the amount of pages to cycle through
                 '''
+                df = pd.DataFrame(columns=['Address','Status','Price','Meta','Bed','Bath','Sqft'])
+
+                statuses = []
+                prices = []
+                metas = []
+                addresses = []
+                beds = []
+                baths = []
+                sqfts = []
+
                 url = 'https://www.realtor.com/realestateandhomes-search/{}/{}/pg-{}'
                 for i in range(1,pages+1):
                         print('**working {}/{}**'.format(i,pages))
@@ -37,10 +42,12 @@ pages - the amount of pages to cycle through
                         browser.get(url.format(form,options,i))
 
                         status = browser.find_elements(By.XPATH,"//div[@class='jsx-3853574337']")
-                        for x in status: statuses.append(x.text)
+                        for x in status: 
+                                statuses.append(x.text)
 
                         meta = browser.find_elements(By.XPATH,"//div[@data-testid='property-meta-container']")
-                        for x in meta: metas.append(x.text)
+                        for x in meta: 
+                                metas.append(x.text)
 
                         price = browser.find_elements(By.XPATH,"//span[@data-label='pc-price']")
                         for x in price: 
@@ -49,7 +56,8 @@ pages - the amount of pages to cycle through
                                 prices.append(p2)
 
                         address = browser.find_elements(By.XPATH,"//div[@data-label='pc-address']")
-                        for x in address: addresses.append(x.text.replace('\n',' '))
+                        for x in address: 
+                                addresses.append(x.text.replace('\n',' '))
 
                         browser.quit()
                         display.stop()
@@ -60,4 +68,26 @@ pages - the amount of pages to cycle through
                 df['Price'] = prices
                 df['Meta'] = metas
 
-                df.to_csv("data/active_homes.csv",index=False)
+                for entry in df['Meta']:
+                        bedI = entry.find("bed")
+                        bathI = entry.find("bath")
+                        sqftI = entry.find("sqft")
+                        beds.append(entry[bedI - 1])
+                        baths.append(entry[bathI - 1])
+                        sqft = ''
+                        i = bathI + 4
+                        
+                        while(i<sqftI):
+                                sqft += entry[i]
+                                i += 1
+                                
+                        sqfts.append(sqft)
+
+                del df['Meta']
+                df['Bed'] = beds
+                df['Bath'] = baths
+                df['Sqft'] = sqfts
+                df.sort_values(by=['Price'],inplace=True)
+                df = df.dropna()
+
+                return df
